3163ddc93785c72d011ba728922de6390c0755db
Hamilton Laws <hamiltonlaws233@gmail.com>
2022-04-15 17:32:41 -0400

    Updated with new GET and POST atributes

--- src/main/java/com/quintrix/springboot/controllers/CarController.java
+++ src/main/java/com/quintrix/springboot/controllers/CarController.java
@@ -3,8 +3,15 @@ package com.quintrix.springboot.controllers;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.quintrix.springboot.controllers.models.Car;
 
@@ -21,5 +28,36 @@ public class CarController {
 
     return carsList;
   }
+
+  @RequestMapping(value = "/cars/{id}", method = RequestMethod.GET)
+  public List<Car> getCarById(@PathVariable("id") Long id) {
+    List<Car> selectedCar;
+
+    selectedCar = carsList.stream().filter(c -> c.getId().equals(id)).collect(Collectors.toList());
+
+    return selectedCar;
+  }
+
+  @RequestMapping(value = "/cars/specificCar", method = RequestMethod.GET)
+  @ResponseBody
+  public List<Car> getCarsModle(@RequestParam("modle") String modle) {
+    List<Car> selectedCar;
+
+    selectedCar =
+        carsList.stream().filter(c -> c.getModle().equals(modle)).collect(Collectors.toList());
+
+    return selectedCar;
+  }
+
+  @RequestMapping(value = "/cars", method = RequestMethod.POST)
+  public ResponseEntity<?> newCar(@RequestBody Car car) {
+
+    Car addedCar = car;
+
+    carsList.add(addedCar);
+
+    return new ResponseEntity<>(addedCar, HttpStatus.OK);
+  }
+
 
 }
